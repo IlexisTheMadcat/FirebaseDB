5ed68a3008bc7332bf464e1b0cdc380cbeea6029
SUPERMECHM500 <50256109+SUPERMECHM500@users.noreply.github.com>
2021-03-04 17:19:59 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="FirebaseDB-dict",
+    version="0.5.0",
+    description="Live interaction to a Firebase Realtime Database.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SUPERMECHM500/FirebaseDB",
+    author="MECH HUB FACTORY",
+    author_email="thedestoryer11@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.5",
+    ],
+    packages=["FirebaseDB"],
+    include_package_data=True,
+    install_requires=["feedparser", "html2text"],
+    }
+)
